a7e4385f45f3909ccb875898d2ef6d245fbd6a40
magicedy <magicedy@gmail.com>
2023-06-21 15:32:41 +0800

    replace by gitea.com

--- install.ps1
+++ install.ps1
@@ -667,11 +667,15 @@ $SCOOP_CONFIG_HOME = $env:XDG_CONFIG_HOME, "$env:USERPROFILE\.config" | Select-O
 $SCOOP_CONFIG_FILE = "$SCOOP_CONFIG_HOME\scoop\config.json"
 
 # TODO: Use a specific version of Scoop and the main bucket
-$SCOOP_PACKAGE_REPO = "https://github.com/ScoopInstaller/Scoop/archive/master.zip"
-$SCOOP_MAIN_BUCKET_REPO = "https://github.com/ScoopInstaller/Main/archive/master.zip"
+# $SCOOP_PACKAGE_REPO = "https://github.com/ScoopInstaller/Scoop/archive/master.zip"
+# $SCOOP_MAIN_BUCKET_REPO = "https://github.com/ScoopInstaller/Main/archive/master.zip"
+$SCOOP_PACKAGE_REPO = "https://gitea.com/magicedy/Scoop/archive/master.zip"
+$SCOOP_MAIN_BUCKET_REPO = "https://gitea.com/magicedy/scoop-bucket-m/archive/master.zip"
 
-$SCOOP_PACKAGE_GIT_REPO = "https://github.com/ScoopInstaller/Scoop.git"
-$SCOOP_MAIN_BUCKET_GIT_REPO = "https://github.com/ScoopInstaller/Main.git"
+# $SCOOP_PACKAGE_GIT_REPO = "https://github.com/ScoopInstaller/Scoop.git"
+# $SCOOP_MAIN_BUCKET_GIT_REPO = "https://github.com/ScoopInstaller/Main.git"
+$SCOOP_PACKAGE_GIT_REPO = "https://gitea.com/magicedy/Scoop.git"
+$SCOOP_MAIN_BUCKET_GIT_REPO = "https://gitea.com/magicedy/scoop-bucket-m.git"
 
 # Quit if anything goes wrong
 $oldErrorActionPreference = $ErrorActionPreference
